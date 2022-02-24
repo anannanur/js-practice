b7b0c2a715e9f19ba0f1790e38322e2d6331d71d
anannanur <anannaanu974@gmail.com>
2022-02-25 02:19:03 +0600

    practising

---
+++ ES6-intro/template-string.js
@@ -0,0 +1,8 @@
+const friend = ['akash','batash', 'hotash','potash','chotash'];
+const name = 'Ananna';
+const live = 'bogura';
+const about = `This is ${name}.
+my friends number ${friend.length +10 *10}.
+I live in ${live}.`;
+
+console.log(about);
