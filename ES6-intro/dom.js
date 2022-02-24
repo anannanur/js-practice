561e493e7bd7d720563548ac0ba2ab7175570c52
anannanur <anannaanu974@gmail.com>
2022-02-24 20:41:09 +0600

    practising

--- ES6-intro/dom.js
+++ ES6-intro/dom.js
@@ -1,30 +1,41 @@
 
-function getElement(element){
-    if(document.getElementsByClassName(element).length != 0){
-        console.log(document.getElementsByClassName(element));
+function getElement(element) {
+    if (document.getElementsByClassName(element).length != 0) {
+        // console.log(document.getElementsByClassName(element));
         return document.getElementsByClassName(element);
     }
-    else{
-        console.log(document.getElementById(element))
+    else {
+        // console.log(document.getElementById(element))
         return document.getElementById(element);
     }
 }
 
-getElement('add-color').addEventListener('click',function(){
+getElement('add-color').addEventListener('click', function () {
     const friends = getElement('friend-name');
-    for(const friend of friends){
+    for (const friend of friends) {
         friend.style.color = 'red';
     }
 })
-getElement('add-background').addEventListener('click',function(){
+getElement('add-background').addEventListener('click', function () {
     const friendContainer = getElement('container');
-        friendContainer.style.backgroundColor = 'lightblue';
-        friendContainer.style.padding = '10px';
-  
+    friendContainer.style.backgroundColor = 'lightblue';
+    friendContainer.style.padding = '10px';
+
 })
-getElement('add-border').addEventListener('click', function(){
+
+getElement('add-friend').addEventListener('click', function () {
+    const friendContainer = getElement('container');
+    const div = document.createElement('div');
+    div.classList.add('friend');
+    div.innerHTML = 
+    `<h2 class="friend-name">New Friend</h2>
+    <p>hello</p>`;
+    friendContainer.appendChild(div);
+
+})
+getElement('add-border').addEventListener('click', function () {
     const friendDiv = getElement('friend');
-    for(const div of friendDiv){
+    for (const div of friendDiv) {
         div.style.border = '1px solid white';
         div.style.borderRadius = '5px';
         div.style.margin = '10px';
