e844ab6db0f7a867e13362da8634ea87b9d08d33
anannanur <anannaanu974@gmail.com>
2022-02-24 19:39:36 +0600

    dom manipulation

--- ES6-intro/dom.js
+++ ES6-intro/dom.js
@@ -1,15 +1,35 @@
-document.getElementById('add-color').addEventListener('click',function(){
-    const friends = document.getElementsByClassName('friend-name');
+
+function getElement(element){
+    if(document.getElementsByClassName(element).length != 0){
+        console.log(document.getElementsByClassName(element));
+        return document.getElementsByClassName(element);
+    }
+    else{
+        console.log(document.getElementById(element))
+        return document.getElementById(element);
+    }
+}
+
+getElement('add-color').addEventListener('click',function(){
+    const friends = getElement('friend-name');
     for(const friend of friends){
         friend.style.color = 'red';
     }
 })
-document.getElementById('add-background').addEventListener('click',function(){
-    const friendDiv = document.getElementsByClassName('friend');
-    console.log(friendDiv);
+getElement('add-background').addEventListener('click',function(){
+    const friendContainer = getElement('container');
+        friendContainer.style.backgroundColor = 'lightblue';
+        friendContainer.style.padding = '10px';
+  
+})
+getElement('add-border').addEventListener('click', function(){
+    const friendDiv = getElement('friend');
     for(const div of friendDiv){
-        console.log(div);
-        div.style.backgroundColor = 'lightblue';
+        div.style.border = '1px solid white';
+        div.style.borderRadius = '5px';
+        div.style.margin = '10px';
         div.style.padding = '10px';
+        div.style.textAlign = 'center';
     }
+
 })
