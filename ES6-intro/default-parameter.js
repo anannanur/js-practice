ca3195f9139b374c04e811715c2d413fd92a404c
anannanur <anannaanu974@gmail.com>
2022-02-25 01:43:39 +0600

    practising

--- ES6-intro/default-parameter.js
+++ ES6-intro/default-parameter.js
@@ -1,4 +1,15 @@
-function add(num1,num2){
+function add(num1,num2 = 5){
+//    if(num2 == undefined){
+//        num2 = 0;
+//    }
+
+    // num2 = num2 || 0;
     return num1 + num2;
 }
-console.log(add(12,10));
+console.log(add(12, 6));
+
+
+function myName(first,last = 'Ananna'){
+    return first + ' ' + last;
+}
+console.log(myName('Humayra'));
