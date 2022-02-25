1b8aa747341416e3f25494e0d1508e071a4e8274
anannanur <anannaanu974@gmail.com>
2022-02-26 01:13:09 +0600

    practising

---
+++ ES6-intro/arrow-function.js
@@ -0,0 +1,17 @@
+function add(num1,num2){
+    return num1+num2;
+}
+// console.log(add(2,3));
+
+const add2 = function add2(num1,num2){
+    return num1+num2;
+}
+// console.log(add2(3,4));
+
+const add3 = function(num1,num2){
+    return num1+num2;
+}
+// console.log(add3(3,5));
+
+const add4 = (num1,num2) => num1 + num2;
+console.log(add4(2,3)); 
